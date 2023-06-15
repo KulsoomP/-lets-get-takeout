e6e73954246b9984b58b95e98bd27162309e0f87
KulsoomP <129038374+KulsoomP@users.noreply.github.com>
2023-06-15 17:18:48 +0530

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,32 @@
+
+
+public class Food implements PricedItem<Integer> {
+    private final String name;
+    private final String description;
+    private int price;
+
+    public Food(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Integer getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: " + name + ", " + description + "     Cost: $" + price;
+    }
+}
---
+++ FoodMenu.java
@@ -0,0 +1,39 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodMenu {
+    private final List<Food> menu;
+
+    public FoodMenu() {
+        this.menu = new ArrayList<>();
+        menu.add(new Food("Burger", "beef patty, cheese, ketchup, mustard", 7));
+        menu.add(new Food("Fries", "deep fried potato wedges", 3));
+        menu.add(new Food("Milkshake", "chocolate, vanilla, strawberry", 5));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < menu.size(); i++) {
+            str.append(i + 1);
+            str.append(" - ");
+            str.append(menu.get(i).toString());
+            str.append("\n");
+        }
+        return str.toString();
+    }
+
+    public Food getFood(int index) {
+        if (menu.get(index) != null) {
+            return menu.get(index);
+        } else {
+            return null;
+        }
+    }
+
+    public int getMenuItemCount() {
+        return menu.size();
+    }
+}
